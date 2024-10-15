9582e97b9c4231acf4ff7725112b793e69b769a9
Robin Mueller <muellerr@irs.uni-stuttgart.de>
2024-10-15 15:10:44 +0200

    better naming and tests update

--- spacepackets/ccsds/time/cds.py
+++ spacepackets/ccsds/time/cds.py
@@ -35,7 +35,7 @@ class CdsShortTimestamp(CcsdsTimeProvider):
     and the size of the time stamp is expected to be seven bytes.
 
     >>> from spacepackets.ccsds.time import CcsdsTimeCodeId
-    >>> cds_short_now = CdsShortTimestamp.from_now()
+    >>> cds_short_now = CdsShortTimestamp.now()
     >>> cds_short_now.len_packed
     7
     >>> hex(cds_short_now.pfield[0])
